9c2a690a855bbc9625496176f0313089b76a8b58
daniel <daniel.zapata.682@gmail.com>
2021-12-10 20:31:06 -0500

    hotfix

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -7,12 +7,18 @@ import { GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  /* apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
+  appId: process.env.REACT_APP_FIREBASE_APPID, */
+  apiKey: "AIzaSyD1zOj7BajIhz8K1rrZoNnnHEpqNPDpH0A",
+  authDomain: "as-722c2.firebaseapp.com",
+  projectId: "as-722c2",
+  storageBucket: "as-722c2.appspot.com",
+  messagingSenderId: "725065191706",
+  appId: "1:725065191706:web:017850eed8ff1c23395ca3",
 };
 
 // Initialize Firebase
